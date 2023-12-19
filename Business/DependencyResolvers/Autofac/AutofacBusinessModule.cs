13c2cd5f05179276f6e60390840635963459c8c9
Alperen Diler <alperendiler1@gmail.com>
2023-12-19 19:35:28 +0300

    Update AutofacBusinessModule

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -13,17 +13,55 @@ namespace Business.DependencyResolvers.Autofac
     {
         protected override void Load(ContainerBuilder builder)
         {
-
-
-
             builder.RegisterType<AnnouncementManager>().As<IAnnouncementService>().SingleInstance();
             builder.RegisterType<EfAnnouncementDal>().As<IAnnouncementDal>().SingleInstance();
+
+
 
             builder.RegisterType<ApplicationManager>().As<IApplicationService>().SingleInstance();
             builder.RegisterType<EfApplicationDal>().As<IApplicationDal>().SingleInstance();
 
             builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
             builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+
+
+            builder.RegisterType<CertificateManager>().As<ICertificateService>().SingleInstance();
+            builder.RegisterType<EfCertificateDal>().As<ICertificateDal>().SingleInstance();
+
+            builder.RegisterType<ClassAnnouncementManager>().As<IClassAnnouncementService>().SingleInstance();
+            builder.RegisterType<EfClassAnnouncementDal>().As<IClassAnnouncementDal>().SingleInstance();
+
+            builder.RegisterType<ClassCourseManager>().As<IClassCourseService>().SingleInstance();
+            builder.RegisterType<EfClassCourseDal>().As<IClassCourseDal>().SingleInstance();
+
+            builder.RegisterType<ClassRoomManager>().As<IClassRoomService>().SingleInstance();
+            builder.RegisterType<EfClassRoomDal>().As<IClassRoomDal>().SingleInstance();
+
+
+            builder.RegisterType<CompanyManager>().As<ICompanyService>().SingleInstance();
+            builder.RegisterType<EfCompanyDal>().As<ICompanyDal>().SingleInstance();
+
+            builder.RegisterType<CompetenceManager>().As<ICompetenceService>().SingleInstance();
+            builder.RegisterType<EfCompetenceDal>().As<ICompetenceDal>().SingleInstance();
+
+
+            builder.RegisterType<CourseManager>().As<ICourseService>().SingleInstance();
+            builder.RegisterType<EfCourseDal>().As<ICourseDal>().SingleInstance();
+
+            builder.RegisterType<EducationManager>().As<IEducationService>().SingleInstance();
+            builder.RegisterType<EfEducationDal>().As<IEducationDal>().SingleInstance();
+
+            builder.RegisterType<ExamManager>().As<IExamService>().SingleInstance();
+            builder.RegisterType<EfExamDal>().As<IExamDal>().SingleInstance();
+
+            builder.RegisterType<PersonalInformationManager>().As<IPersonalInformationService>().SingleInstance();
+            builder.RegisterType<EfPersonalInformationDal>().As<IPersonalInformationDal>().SingleInstance();
+
+            builder.RegisterType<SocialMediaManager>().As<ISocialMediaService>().SingleInstance();
+            builder.RegisterType<EfSocialMediaDal>().As<ISocialMediaDal>().SingleInstance();
+
+            builder.RegisterType<StudentLanguageManager>().As<IStudentLanguageService>().SingleInstance();
+            builder.RegisterType<EfStudentLanguageDal>().As<IStudentLanguageDal>().SingleInstance();
 
             builder.RegisterType<StudentManager>().As<IStudentService>().SingleInstance();
             builder.RegisterType<EfStudentDal>().As<IStudentDal>().SingleInstance();
@@ -33,39 +71,17 @@ namespace Business.DependencyResolvers.Autofac
 
             builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
             builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
-            builder.RegisterType<PersonalInformationManager>().As<IPersonalInformationService>().SingleInstance();
-            builder.RegisterType<EfPersonalInformationDal>().As<IPersonalInformationDal>().SingleInstance();
-            builder.RegisterType<SocialMediaManager>().As<ISocialMediaService>().SingleInstance();
-            builder.RegisterType<EfSocialMediaDal>().As<ISocialMediaDal>().SingleInstance();
-            builder.RegisterType<StudentLanguageManager>().As<IStudentService>().SingleInstance();
-            builder.RegisterType<EfStudentDal>().As<IStudentDal>().SingleInstance();
-
-          
-
-            builder.RegisterType<ClassCourseManager>().As<IClassCourseService>().SingleInstance();
-            builder.RegisterType<EfClassCourseDal>().As<IClassCourseDal>().SingleInstance();
-
-            builder.RegisterType<CertificateManager>().As<ICertificateService>().SingleInstance();
-            builder.RegisterType<EfCertificateDal>().As<ICertificateDal>().SingleInstance();
-
-            builder.RegisterType<ClassAnnouncementManager>().As<IClassAnnouncementService>().SingleInstance();
-            builder.RegisterType<EfClassAnnouncementDal>().As<IClassAnnouncementDal>().SingleInstance();
-            builder.RegisterType<PersonalInformationManager>().As<IPersonalInformationService>().SingleInstance();
-            builder.RegisterType<EfPersonalInformationDal>().As<IPersonalInformationDal>().SingleInstance();
-            builder.RegisterType<SocialMediaManager>().As<ISocialMediaService>().SingleInstance();
-            builder.RegisterType<EfSocialMediaDal>().As<ISocialMediaDal>().SingleInstance();
-            builder.RegisterType<StudentLanguageManager>().As<IStudentService>().SingleInstance();
-            builder.RegisterType<EfStudentDal>().As<IStudentDal>().SingleInstance();
 
 
-            builder.RegisterType<ClassCourseManager>().As<IClassCourseService>().SingleInstance();
-            builder.RegisterType<EfClassCourseDal>().As<IClassCourseDal>().SingleInstance();
 
-            builder.RegisterType<CertificateManager>().As<ICertificateService>().SingleInstance();
-            builder.RegisterType<EfCertificateDal>().As<ICertificateDal>().SingleInstance();
 
-            builder.RegisterType<ClassAnnouncementManager>().As<IClassAnnouncementService>().SingleInstance();
-            builder.RegisterType<EfClassAnnouncementDal>().As<IClassAnnouncementDal>().SingleInstance();
+
+
+
+
+
+
+
 
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
 
