38e41c21497e482eb552a4b8ae50da7f1abb6e67
Busra <b.ertas91@gmail.com>
2023-12-19 18:56:58 +0300

    ClassRoom,Company,Competence Validator/Autofac is added

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -3,6 +3,7 @@ using Autofac.Extras.DynamicProxy;
 using Business.Abstracts;
 using Business.Concretes;
 using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
 using DataAccess.Abstracts;
 using DataAccess.Concretes;
 
@@ -18,7 +19,14 @@ namespace Business.DependencyResolvers.Autofac
             builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
             builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
 
-            
+            builder.RegisterType<ClassRoomManager>().As<IClassRoomService>().SingleInstance();
+            builder.RegisterType<EfClassRoomDal>().As<IClassRoomDal>().SingleInstance();
+
+            builder.RegisterType<CompanyManager>().As<ICompanyService>().SingleInstance();
+            builder.RegisterType<EfCompanyDal>().As<ICompanyDal>().SingleInstance();
+
+            builder.RegisterType<CompetenceManager>().As<ICompetenceService>().SingleInstance();
+            builder.RegisterType<EfCompetenceDal>().As<ICompetenceDal>().SingleInstance();
 
 
 
--- Business/ValidationRules/FluentValidation/UserRequestValidator.cs
+++ Business/ValidationRules/FluentValidation/UserRequestValidator.cs
@@ -1,12 +1,6 @@
 ﻿using Business.Constants;
 using Business.Dtos.Users.Requests;
-using Entities.Concretes;
 using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Business.ValidationRules.FluentValidation
 {
@@ -19,11 +13,11 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(r => r.Email).NotEmpty().EmailAddress();
             RuleFor(r => r.PasswordSalt).MinimumLength(8).MaximumLength(10).NotEmpty().WithMessage(Messages.MustContainAtMinTwoChar).WithMessage(Messages.MustContainAtMaxTenChar);
 
-           
+
 
 
         }
 
-        
+
     }
 }
