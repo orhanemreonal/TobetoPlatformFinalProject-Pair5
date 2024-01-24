37616de46d774f3cbf024247209d305c450149de
Busra <b.ertas91@gmail.com>
2024-01-24 10:18:24 +0300

    DbSets are added

--- DataAccess/Context/TobetoPlatformContext.cs
+++ DataAccess/Context/TobetoPlatformContext.cs
@@ -1,4 +1,6 @@
 ﻿using Core.Entities.Concrete;
+using Entities;
+using Entities.Concretes;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System.Reflection;
@@ -8,6 +10,32 @@ namespace DataAccess.Context
     public class TobetoPlatformContext : DbContext
     {
         protected IConfiguration Configuration { get; set; }
+
+        public DbSet<Announcement> Announcements { get; set; }
+        public DbSet<Application> Applications { get; set; }
+        public DbSet<AsyncVideo> AsyncVideo { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Classroom> Classrooms { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Certificate> Certificates { get; set; }
+        public DbSet<Competence> Competences { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<PersonalInformation> PersonalInformations { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<StudentLanguage> StudentLanguages { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Survey> Surveys { get; set; }
+        public DbSet<Title> Titles { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<VirtualClass> VirtualClasses { get; set; }
 
         public DbSet<OperationClaim> OperationClaims { get; set; }
 
